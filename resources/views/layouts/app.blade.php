83a5b9e93ca2c2074e8a7fe26ce0ea9d81553f72
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2021-03-24 12:49:16 +0100

    loader added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,15 @@
     @yield('content')
 
 
+    {{-- Fixed buttons --}}
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    {{-- loader --}}
+    <div id="loader">
+        <img src="{{ url('images/logo.jpg') }}" alt="Company logo">
+    </div>
+
+
     {{-- ========== JS Libraries ========== --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js">
